e0d65a7c14c26679a3b9b6f22520fa086110950e
silvareal <sylvernusakubo@yahoo.com>
2018-03-14 12:38:00 +0100

    Cling used

--- coreapi/wsgi.py
+++ coreapi/wsgi.py
@@ -10,9 +10,9 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coreapi.settings")
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = Cling(get_wsgi_application())
+
